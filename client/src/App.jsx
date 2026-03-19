ad0eda07ebaee99916f07ceec60d6fcdd8d8aa55
Aditya Raj Sharma <adityarajsharma7360@gmail.com>
2026-03-19 22:50:07 +0530

    Initial commit

---
+++ client/src/App.jsx
@@ -0,0 +1,32 @@
+import { Route, HashRouter as Router, Routes } from "react-router-dom";
+import Hero from "./components/Hero";
+import Navbar from "./components/Navbar";
+import About from "./pages/About";
+import Blog from "./pages/Blog";
+import Cart from "./pages/Cart";
+import Collection from "./pages/Collection";
+import FAQ from "./pages/FAQ";
+import Product from "./pages/Product";
+import Profile from "./pages/Profile";
+
+function App() {
+  return (
+    <div className="font-sans text-primary">
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Hero />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/blog" element={<Blog />} />
+          <Route path="/faq" element={<FAQ />} />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/cart" element={<Cart />} />
+          <Route path="/collections" element={<Collection />} />
+          <Route path="/product" element={<Product />} />
+        </Routes>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
